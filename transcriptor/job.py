5c8702469f1d514f636934a9d1fdeb9b5a032608
kjaymiller <kjaymiller@gmail.com>
2020-05-09 12:07:15 -0700

    remove dataclass from Job

--- transcriptor/job.py
+++ transcriptor/job.py
@@ -1,13 +1,23 @@
-from dataclasses import dataclass
 from transcriptor.speakers import Speaker
 from transcriptor.markers import Marker
 
 import typing
 
-@dataclass
 class Job:
-    base_text: str
-    name: str
-    markers: typing.List
-    transcription: typing.Dict
-    speakers: typing.List
+    def __init__(
+            self,
+            name: str,
+            base_text: str,
+            markers: typing.List,
+            transcription: typing.Dict,
+            speakers: typing.List,
+            ):
+
+        self.name = name
+        self.base_text = base_text
+        self.markers = markers
+        self.transcription = transcription
+        self.speakers = speakers
+
+    def text_at_marker(self, marker:Marker) -> str:
+        pass
